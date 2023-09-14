c3a2510a1a7311397fdc4faf238b0321d40503f8
Petr Savelev <petr.savelev.direct@gmail.com>
2023-09-14 13:40:27 +0300

    Site content and copyright

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
+import { MaterialModule } from './material.module';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { MainHeaderComponent } from './main-header/main-header.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
     MainHeaderComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/copyright/copyright.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'copyright',
+  templateUrl: './copyright.component.html',
+  styleUrls: ['./copyright.component.less']
+})
+export class CopyrightComponent {
+
+}
