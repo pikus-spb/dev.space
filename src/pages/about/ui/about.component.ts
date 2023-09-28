eb8935c78495c64819c11e2a25c7a607a1e16772
Petr Savelev <petr.savelev.direct@gmail.com>
2023-09-28 20:51:55 +0300

    Updated content pages

--- src/pages/about/ui/about.component.ts
+++ src/pages/about/ui/about.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.less'],
+  imports: [MatChipsModule],
   standalone: true,
 })
 export class AboutComponent {}
--- src/pages/links/ui/links.component.ts
+++ src/pages/links/ui/links.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-links',
   templateUrl: './links.component.html',
   styleUrls: ['./links.component.less'],
+  imports: [MatListModule],
   standalone: true,
 })
 export class LinksComponent {}
